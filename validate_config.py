9d92d180f36e5908aac6e9e7b3133e7b90af8369
root <root@panda.techn.3s.pl>
2020-02-18 12:17:37 +0000

    added basic reporting

--- validate_config.py
+++ validate_config.py
@@ -4,6 +4,7 @@ import socket
 import json
 from netmiko import ConnectHandler
 import textfsm
+from jinja2 import Template
 
 def remove_vty_acls(acl_list: str, netmiko_handler: ConnectHandler):
    '''
@@ -41,23 +42,35 @@ def main():
         devices = json.load(f)
     with open('inventory-new.json', 'r') as file2:
         devices_meta = json.load(file2)
-  
+    report_devices = []
     # Go over all devices in our netmiko compatible inventory
     for device in devices:
         # For current device, look up additional metadata 
             print(f"Now working on {device['host']}...")
             if devices_meta[device['host']].get('nos') == "iosxr":
+                print("Skipping IOS XR...")
                 continue
-            try:
-                handler = ConnectHandler(**device)
-#               handler.send_command("terminal length 0")
-                # Get configured TACACS servers
-                print(get_tacacs(handler))     
-                # Get configured ACLs on line VTYs
-                print(get_vty_acls(handler, devices_meta[device['host']]))
-        
-            except Exception as e:
-                print(e)    
+            elif devices_meta[device['host']].get('pop_street') == "Ligocka":
+                try:
+                    handler = ConnectHandler(**device)
+                    # Get configured TACACS servers
+                    tacacs = get_tacacs(handler)
+                    # Get configured ACLs on line VTYs
+                    vty_acls = get_vty_acls(handler, devices_meta[device['host']])
+                    current_device = {}
+                    t = []
+                    current_device['hostname'] = device['host']
+                    for tac in tacacs:
+                        t.append(tac['tacacs_server'])
+                    current_device['tacacs'] = t
+                    report_devices.append(current_device)
+                except Exception as e:
+                    print(e)    
+    # Generate report
+    jinja2_template = open("report.html", "r").read()
+    template = Template(jinja2_template)
+    rendered_template = template.render(devices = report_devices)
+    print(rendered_template)
 
 if __name__ == '__main__':
 
